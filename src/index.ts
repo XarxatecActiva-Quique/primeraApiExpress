813b815e52f9fe7cfc0684f2d4b6f139917edd9c
Enrique Serrano Batalla <enrique.serrano@xarxatecactiva.com>
2024-10-17 11:10:52 +0200

    fix: add type:module in package.json

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = Express();
 const port = 3000;
 
 app.get('/', (req:Express.Request, res:Express.Response) => {
-    res.send('Hello World!');
+    res.send('<h1>Hello World!</h1>');
 })
 
 app.listen(port, () => {
